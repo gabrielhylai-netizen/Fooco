d5a9d3c24dca43f5ed37d8e7821bee647c21f9b9
gabrielhylai-netizen <gabrielhylai@gmail.com>
2026-08-21 15:56:48 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'foco-v8';
+const CACHE = 'foco-v10';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
